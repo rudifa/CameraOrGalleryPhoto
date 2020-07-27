ea8a0901be38ed8e33e19bca2045609e1a4ecada
Rudi Farkas <rudi.farkas@gmail.com>
2020-07-27 21:25:55 +0200

    update to swift 5

--- CameraOrGalleryPhoto/SimpleImagePicker.swift
+++ CameraOrGalleryPhoto/SimpleImagePicker.swift
@@ -35,9 +35,9 @@ class SimpleImagePicker: NSObject, UIImagePickerControllerDelegate, UINavigation
 
     func openCamera()
     {
-        if (UIImagePickerController .isSourceTypeAvailable(UIImagePickerControllerSourceType.camera))
+        if (UIImagePickerController .isSourceTypeAvailable(UIImagePickerController.SourceType.camera))
         {
-            imagePicker.sourceType = UIImagePickerControllerSourceType.camera
+            imagePicker.sourceType = UIImagePickerController.SourceType.camera
             imagePicker.allowsEditing = true
             owner?.present(imagePicker, animated: true, completion: nil)
         }
@@ -51,17 +51,30 @@ class SimpleImagePicker: NSObject, UIImagePickerControllerDelegate, UINavigation
 
     func openGallery()
     {
-        imagePicker.sourceType = UIImagePickerControllerSourceType.photoLibrary
+        imagePicker.sourceType = UIImagePickerController.SourceType.photoLibrary
         imagePicker.allowsEditing = true
         owner?.present(imagePicker, animated: true, completion: nil)
     }
 
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+// Local variable inserted by Swift 4.2 migrator.
+let info = convertFromUIImagePickerControllerInfoKeyDictionary(info)
+
         picker.dismiss(animated: true, completion: nil)
         //You will get cropped image here..
-        if let image = info[UIImagePickerControllerEditedImage] as? UIImage
+        if let image = info[convertFromUIImagePickerControllerInfoKey(UIImagePickerController.InfoKey.editedImage)] as? UIImage
         {
             delegate?.selected(image: image)
         }
     }
+}
+
+// Helper function inserted by Swift 4.2 migrator.
+fileprivate func convertFromUIImagePickerControllerInfoKeyDictionary(_ input: [UIImagePickerController.InfoKey: Any]) -> [String: Any] {
+	return Dictionary(uniqueKeysWithValues: input.map {key, value in (key.rawValue, value)})
+}
+
+// Helper function inserted by Swift 4.2 migrator.
+fileprivate func convertFromUIImagePickerControllerInfoKey(_ input: UIImagePickerController.InfoKey) -> String {
+	return input.rawValue
 }
